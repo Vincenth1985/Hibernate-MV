7de37a61ed40e8696728d317a64af84830b359e2
Vincent Honca <vincenth1985@gmail.com>
2019-09-30 09:43:48 +0200

    main extends application

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.nio.file.Paths;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent parent = FXMLLoader.load(Paths.get("src/main/resources/Fxml/mvhibernate.fxml").toUri().toURL());
+        stage.setScene(new Scene(parent, 600, 400));
+        stage.setTitle("Manu & Vincent");
+        stage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        launch();
+    }
+}
