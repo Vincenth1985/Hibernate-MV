fe39a8740aa50cc3e3f7d4fc4010fa3f43e94b91
izunatan <ffxivizunatan@gmail.com>
2019-09-30 11:23:47 +0200

    Created all WC methods (not implemented)

--- src/main/java/view/WindowController.java
+++ src/main/java/view/WindowController.java
@@ -1,7 +1,128 @@
 package view;
 
+import javafx.fxml.FXML;
+
 public class WindowController {
 
+    // --------------------- CREATE ---------------------
+    @FXML
+    private void createClient() {
 
+    }
 
+    @FXML
+    private void createOffer() {
+
+    }
+
+    @FXML
+    private void createProduct() {
+
+    }
+
+    // --------------------- GET ---------------------
+    @FXML
+    private void getClient() {
+
+    }
+
+    @FXML
+    private void getOffer() {
+
+    }
+
+    @FXML
+    private void getOrder() {
+
+    }
+
+    @FXML
+    private void getProduct() {
+
+    }
+
+    // --------------------- DELETE ---------------------
+    @FXML
+    private void deleteOffer() {
+
+    }
+
+    @FXML
+    private void deleteOrder() {
+
+    }
+
+    @FXML
+    private void deleteClient() {
+
+    }
+
+    @FXML
+    private void deleteProduct() {
+
+    }
+
+    // --------------------- PRINT ---------------------
+    @FXML
+    private void printClient() {
+
+    }
+
+    @FXML
+    private void printProduct() {
+
+    }
+
+    @FXML
+    private void printOrder() {
+
+    }
+
+    @FXML
+    private void printOffer() {
+
+    }
+
+    // --------------------- UPDATE ---------------------
+    @FXML
+    private void updateProduct() {
+
+    }
+
+    @FXML
+    private void updateClient() {
+
+    }
+
+    @FXML
+    private void updateOrder() {
+
+    }
+
+    @FXML
+    private void updateOffer() {
+
+    }
+
+    // --------------------- ADD ---------------------
+    @FXML
+    private void addClientToOffer() {
+
+    }
+
+    @FXML
+    private void addProductsToClient() {
+
+    }
+
+    // --------------------- REMOVE ---------------------
+    @FXML
+    private void removeProductsFromOffer() {
+
+    }
+
+    // --------------------- CONFIRM ---------------------
+    @FXML private void confirmOrder() {
+
+    }
 }
